10d8eed7dd3c31c835c3a4e40ae1c8a39635bf86
aymanzaveri <aymaannntariq@gmail.com>
2023-10-12 14:23:22 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+export default async function ATM(User) {
+    // console.clear();
+    while (true) {
+        async function Prompt_ATM() {
+            const result = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action_type',
+                    message: 'ATM Menu:',
+                    choices: ['Check Balance', 'Withdraw', 'Deposit', 'Exit'],
+                    filter: (val) => val.toUpperCase(),
+                }
+            ]);
+            await ATM_Action(result.action_type);
+        }
+        ;
+        await Prompt_ATM();
+    }
+    async function ATM_Action(Action = "DEPOSIT") {
+        switch (Action) {
+            case "EXIT":
+                console.log("Thanks! for using me, have a good day.");
+                process.exit(0);
+                break;
+            case "WITHDRAW":
+                const { withdraw } = await inquirer.prompt([
+                    {
+                        type: 'number',
+                        name: 'withdraw',
+                        message: 'Amount : ',
+                    }
+                ]);
+                if (withdraw > User.balance) {
+                    console.log(chalk.red("Insufficient balance."));
+                }
+                else if ((withdraw > 0) && (withdraw <= User.balance)) {
+                    User = { ...User, balance: User.balance - withdraw };
+                    console.log(`${withdraw} has been withdraw from your account.`);
+                    ATM_Action("");
+                }
+                else {
+                    console.log(chalk.red("Error Occured! Please try again later."));
+                }
+                break;
+            case "DEPOSIT":
+                const { deposit } = await inquirer.prompt([
+                    {
+                        type: 'number',
+                        name: 'deposit',
+                        message: 'Amount : ',
+                    }
+                ]);
+                if ((deposit > 0)) {
+                    User = { ...User, balance: User.balance + deposit };
+                    console.log(`${deposit} has been deposited in your account.`);
+                    ATM_Action("");
+                }
+                else {
+                    console.log(chalk.red("Error Occured! Please try again later."));
+                }
+                break;
+            default:
+                console.log(`Your balance is : ${User.balance}`);
+                break;
+        }
+    }
+}
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
